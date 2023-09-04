4c12a5a639d1bf1d2eb6ee131127f26605b9d8f6
mrs.stjuste <brianstjuste2021@gmail.com>
2023-09-03 23:25:01 -0400

    initial commit

--- src/BalanceEquiry.java
+++ src/BalanceEquiry.java
@@ -1,14 +1,2 @@
-package ASimulatorSystem;
-
-import java.awt.*;
-import java.awt.event.*;
-import java.sql.ResultSet;
-import javax.swing.*;
-import java.util.*;
-
-
-class BalanceEnquiry extends JFrame implements ActionListener {
-
-
-
+public class BalanceEquiry {
 }
